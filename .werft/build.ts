bdd3d49c1da480673fa26e5d25c62d0308ee6869
Wulf Thimm <wulf.thimm@typefox.io>
2021-08-27 13:31:03 +0000

    dev

--- .werft/build.ts
+++ .werft/build.ts
@@ -409,6 +409,7 @@ export async function deployToDev(deploymentConfig: DeploymentConfig, workspaceF
             flags += ` -f ../.werft/values.dev.gcp-storage.yaml`;
         }
 
+
         exec(`helm dependencies up`);
         exec(`/usr/local/bin/helm3 upgrade --install --timeout 10m -f ../.werft/${nodeAffinityValues} -f ../.werft/values.dev.yaml ${flags} ${helmInstallName} .`);
         exec(`kubectl apply -f ../.werft/jaeger.yaml`);
